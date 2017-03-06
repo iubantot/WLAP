ed62924fb27467c1025ea6fa519cdf5d166179e1
j0a0c <johnalvinchiu1995@gmail.com>
2017-03-06 11:10:49 +0800

    Updated WLAP course

--- facHome.php
+++ facHome.php
@@ -25,6 +25,15 @@
 				background:none !important;
 				background-color: #fff !important;
 			}
+			.container-pdf * > .modal-body{
+				width:100%;
+				height: calc(100vh - 125px);
+
+			}
+			.pdfobject-container{
+				width:100%;
+				height: calc(100vh - 155px);
+			}
 		</style>
 
 		<link href="css/bootstrap.min.css" rel="stylesheet">
